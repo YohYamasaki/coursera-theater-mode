388ab83bbfd3300728c563d06788beb3ae737992
Yohei <74522538+YohyYamasaki@users.noreply.github.com>
2024-12-31 18:20:29 +0100

    WIP: added page change detector

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import { TheaterButtonSvg } from "./assets/svg.ts";
-import { waitForElement } from "./utils.ts";
+import {TheaterButtonSvg} from "./assets/svg.ts";
+import {waitForElement} from "./utils.ts";
 const SCROLL_MARGIN_PX = 16;
 
-async function main() {
+async function main(): Promise<void> {
+  const bodyWrapSelector = ".rc-LegacyDataFetch";
+  const bodyWrapEl = await waitForElement(bodyWrapSelector);
+
+  // Observe change of bodyWrapEl to attach the button for every page navigation
+  const bodyWrapObserver = new MutationObserver(addTogglebutton);
+  if (bodyWrapEl) {
+    bodyWrapObserver.observe(bodyWrapEl, {childList: true});
+  }
+}
+
+async function addTogglebutton(): Promise<void> {
   // Wait until all necessary elements become available
-  const [mainContainerEl, titleEl, videoEl, toolbarEl] = (await Promise.all([
+  const [mainContainerEl, toolbarEl] = (await Promise.all([
     waitForElement("#main-container"),
-    waitForElement("#video-item-title-and-save-note"),
-    waitForElement(".video-js video.vjs-tech"),
     waitForElement(".rc-ControlBar .icon-container"),
   ])) as (HTMLElement | null)[];
+
+  // Current state
+  let isTheaterMode = false;
 
   // Add theater mode button
   const buttonEl = document.createElement("button");
@@ -17,27 +29,20 @@ async function main() {
   buttonEl.innerHTML = TheaterButtonSvg;
   buttonEl.onclick = () => {
     mainContainerEl?.classList.toggle("theater-mode");
+    isTheaterMode = !isTheaterMode;
   };
   const buttonContainerEl = document.createElement("span");
   buttonContainerEl.className = "rc-TooltipWrapper";
   buttonContainerEl.append(buttonEl);
   toolbarEl?.append(buttonContainerEl);
 
-  // Scroll handling to change the video height
-  const originalPos = titleEl?.getBoundingClientRect();
-
   function handleScroll() {
-    if (!titleEl || !videoEl || !originalPos) return;
-    if (!mainContainerEl || !mainContainerEl.classList.contains("theater-mode"))
-      return;
+    if (!mainContainerEl || !isTheaterMode) return;
 
-    // Set shorter height to create a space for transcripts when scrolled
-    const newPos = titleEl.getBoundingClientRect();
-
-    if (newPos.top + SCROLL_MARGIN_PX < originalPos.top) {
-      videoEl.style.height = "400px";
+    if (mainContainerEl.scrollTop >= SCROLL_MARGIN_PX) {
+      mainContainerEl.classList.remove("theater-mode");
     } else {
-      videoEl.style.height = "100%";
+      mainContainerEl.classList.add("theater-mode");
     }
   }
 
--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,9 @@
  * @param selector CSS selector of the target
  * @returns Promise of Element
  */
-export function waitForElement(selector: string): Promise<Element | null> {
+export async function waitForElement(
+  selector: string
+): Promise<Element | null> {
   return new Promise((resolve) => {
     const observer = new MutationObserver((_, observer) => {
       const element = document.querySelector(selector);
@@ -19,3 +21,18 @@ export function waitForElement(selector: string): Promise<Element | null> {
     });
   });
 }
+
+// export function detectElementChange(selector: string) {
+//   const observer = new MutationObserver((mutation) => {
+//     console.log(mutation);
+//   });
+
+//   observer.observe(document.querySelector("#main-container")!, {
+//     attributes: true,
+//     characterData: true,
+//     childList: true,
+//     subtree: true,
+//     attributeOldValue: true,
+//     characterDataOldValue: true,
+//   });
+// }
